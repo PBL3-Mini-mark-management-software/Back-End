784e0df3084710d956ce8ba3cb58b1d13886dbfc
Huỳnh Đoàn Thanh Tuyền <147983106+mypiett@users.noreply.github.com>
2025-05-10 14:30:40 +0700

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/DTO/CustomerRequest.java
@@ -0,0 +1,12 @@
+package com.example.backend.DTO;
+
+import java.time.LocalDate;
+
+public class CustomerRequest {
+    public String name;
+    public String phone;
+    public LocalDate date_of_birth;
+    public Integer points;
+    public String membership_type;
+    public String customer_group;
+}
