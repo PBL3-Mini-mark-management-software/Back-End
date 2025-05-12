07e477ce2718782f0e2b4ec993b37a4ac03d036f
Huỳnh Đoàn Thanh Tuyền <147983106+mypiett@users.noreply.github.com>
2025-05-12 11:07:15 +0700

    Update EmployeeRepository.java

--- backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
+++ backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
@@ -1,7 +1,11 @@
 package com.example.backend.repository;
 import com.example.backend.model.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeRepository extends JpaRepository<Employees, String> {
+    boolean existsByPhone(String phone);
+    @Query("SELECT MAX(e.ind) FROM Employees e")
+    Long findMaxInd();
 }
 
