6e61cdaa52fb5f1cc0697fbcf201b001c57d41e9
Huỳnh Đoàn Thanh Tuyền <147983106+mypiett@users.noreply.github.com>
2025-05-12 11:04:28 +0700

    Update Suppliers.java

--- backend/src/main/java/com/example/backend/model/Suppliers.java
+++ backend/src/main/java/com/example/backend/model/Suppliers.java
@@ -5,20 +5,22 @@ import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 @Entity
 @Getter
 @Setter
 public class Suppliers {
     @Id
-    private String supplierId;
+    private String supplier_id;
     private String name;
+    
+    @Column(unique = true)
     private String phone;
+    
     private String address;
 
-    @OneToMany(mappedBy = "supplier")
-@JsonIgnoreProperties("supplier")
-private List<Products> products;
+    @Column(unique = true)
+    private Long ind; 
 
+    @OneToMany(mappedBy = "supplier")
+    private List<Products> product;
 }
