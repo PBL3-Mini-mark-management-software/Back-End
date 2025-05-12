1059ba987210d78d6a2a9c383b3a490f734e94a5
Huỳnh Đoàn Thanh Tuyền <147983106+mypiett@users.noreply.github.com>
2025-05-12 11:01:16 +0700

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/DTO/EmployeeRequest.java
@@ -0,0 +1,13 @@
+package com.example.backend.DTO;
+
+import java.time.LocalDate;
+
+public class EmployeeRequest {
+    public String name;
+    public String phone;
+    public LocalDate date_of_birth;
+    public String user_name;
+    public String password;
+    public String position;
+    public Integer salary;
+}
---
+++ backend/src/main/java/com/example/backend/DTO/SupplierRequest.java
@@ -0,0 +1,7 @@
+package com.example.backend.DTO;
+
+public class SupplierRequest {
+    public String name;
+    public String phone;
+    public String address;
+}
