815053139351a4df8e5186387676bea049e706a7
Huỳnh Đoàn Thanh Tuyền <147983106+mypiett@users.noreply.github.com>
2025-05-12 11:06:40 +0700

    Update Employees.java

--- backend/src/main/java/com/example/backend/model/Employees.java
+++ backend/src/main/java/com/example/backend/model/Employees.java
@@ -3,6 +3,8 @@ package com.example.backend.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
@@ -10,13 +12,21 @@ import java.util.List;
 @Setter
 public class Employees {
     @Id
-    private String employeeId;
+    private String employee_id;
+
+    @Column(unique = true)
+    private String phone; 
+
+
     private String name;
     private String position;
-    private String userName;
+    private String user_name;
     private String password;
-    private String phone; // sửa thành String
     private Integer salary;
+    private LocalDate date_of_birth;
+
+    @Column(unique = true)
+    private Long ind; 
 
     @OneToMany(mappedBy = "employee")
     private List<Orders> order;
